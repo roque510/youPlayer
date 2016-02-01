5dd821eb8cd8203ef6a61356fbeaa24d2d267351
Armando <roque510@hotmail.com>
2016-01-31 18:47:09 -0600

    cambio de skin en player y adicion de modulo music

--- js/mainplayer.js
+++ js/mainplayer.js
@@ -1,3 +1,12 @@
+function setMedia(URL,NAME){
+    
+     $('#musiconPlayer').jPlayer('setMedia', {
+     title:NAME,
+      m4a: URL
+   });
+     $('#musiconPlayer').jPlayer("play"); 
+}
+
 $('.btnP').click(function() {
     
     $("#musiconPlayer").jPlayer("setMedia", { // Set the media
@@ -6,25 +15,57 @@ $('.btnP').click(function() {
     }).jPlayer("play");
 });
 
-$(document).ready(function() {
 
-    $("#musiconPlayer").jPlayer({
-        ready: function(event) {
-            $(this).jPlayer("setMedia", {
-				title: "The Beatles",
-				m4a: "http://listen.radionomy.com/liverpool-revolution-live"
-				
-            });
+(function localFileVideoPlayerInit(win) {
+    
+    var URL = win.URL || win.webkitURL,
+        displayMessage = (function displayMessageInit() {
+            var node = document.getElementById('message');
+            
+            return function displayMessage(message, isError) {
+                node.innerHTML = message;
+                node.className = isError ? 'error' : 'info';
+            };
+        }()),
+        playSelectedFile = function playSelectedFileInit(event) {
+          
+            var file = this.files[0];
+
+            var type = file.type;
+
+            var name = file.name;
+
+            
+
+            var videoNode = document.querySelector('audio');
+
+            var canPlay = videoNode.canPlayType(type);
+
+            canPlay = (canPlay === '' ? 'no' : canPlay);
+
+            var message = 'Can play type "' + type + '": ' + canPlay;
+
+            var isError = canPlay === 'no';
+
+            displayMessage(message, isError);
+
+            if (isError) {
+                return;
+            }
+
+            var fileURL = URL.createObjectURL(file);
+
+            setMedia(fileURL,name);
+
+            //videoNode.src = fileURL;
         },
+        inputNode = document.getElementById('input');                                        
+    if (!URL) {
+        displayMessage('Your browser is not ' + 
+           '<a href="http://caniuse.com/bloburls">supported</a>!', true);
         
-        swfPath: "http://jplayer.org/latest/dist/jplayer",
-        supplied: "m4a",
-		wmode: "window",
-		useStateClassSkin: true,
-		autoBlur: false,
-		smoothPlayBar: true,
-		keyEnabled: true,
-		remainingDuration: true,
-		toggleDuration: true
-    });
-});   
+        return;
+    }
+    
+    inputNode.addEventListener('change', playSelectedFile, false);
+}(window));     
